929bbba34f21c9553a851a3b9ad7f58df6fb0a48
Yakup Fakhardinov <y.fakhardinov@start.ru>
2024-11-24 20:51:58 +0300

    1

--- client.py
+++ client.py
@@ -1,21 +1,36 @@
+import json
+from pprint import pprint
+
 import requests
 
 
 
 """Создание объявления"""
-response = requests.post(
-    url="http://127.0.0.1:5000/create_advert",
-    json={
-        "title": "Продам python",
-        "description": "Заберите пожалуйста =)",
-        "owner": "super_user"
-    },
-)
-print(response.text)
+# response = requests.post(
+#     url="http://127.0.0.1:5000/create_advert",
+#     json={
+#         "title": "Продам python2",
+#         "description": "Заберите пожалуйста =)",
+#         "owner": "super_user"
+#     },
+# )
+# print(response.text)
 
 
 """Запрос объявлений"""
+response = requests.get(
+    url="http://127.0.0.1:5000/list_adverts?all=true",
+)
+pprint(json.loads(response.text))
 
 """Запрос конкретного объявления"""
+# response = requests.get(
+#     url="http://127.0.0.1:5000/advert/1",
+# )
+# pprint(json.loads(response.text))
 
-"""Удаление объявления"""
+# """Удаление объявления"""
+# response = requests.delete(
+#     url="http://127.0.0.1:5000/advert/1",
+# )
+# print(response.text)
--- server.py
+++ server.py
@@ -1,15 +1,18 @@
+
 import flask
 from flask import Flask
 from flask import jsonify, request
 from flask.views import MethodView
 from models import Adverts, Session
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy import select
 
 app = Flask("adverts")
 
+
 class HttpError(Exception):
     def __init__(self, status_code: int, error_msg: str|dict|list):
-        self.status_code= status_code
+        self.status_code = status_code
         self.error_msg = error_msg
 
 
@@ -31,6 +34,7 @@ def after_request(http_response: flask.Response):
     request.session.close()
     return http_response
 
+
 def create_advert(advert: Adverts):
     try:
         request.session.add(advert)
@@ -43,28 +47,51 @@ def create_advert(advert: Adverts):
     return advert
 
 
-def get_advert(advert: Adverts, session):
-    pass
+def get_advert(advert_id: int):
+    advert = request.session.get(Adverts, advert_id)
+    if advert is None:
+        raise HttpError(404, "advert not found")
+    return advert
 
 
-class AdveretsView(MethodView):
-    def get(self):
-        pass
+
+class AdvertsView(MethodView):
+    def get(self, advert_id: int = None):
+        if advert_id:
+            advert = get_advert(advert_id)
+            return jsonify(advert.json)
+        else:
+            qs = dict(request.args)
+            for k, v in qs.items():
+                if k == "all" and v == ("true" or True):
+                    all_advert = Session().query(Adverts, Adverts.id, Adverts.title).all()
+                    a = {}
+                    for i in all_advert:
+                        a[i[1]] = (i[2])
+                    if len(a) == 0:
+                        raise HttpError(404, "No adverts created yet")
+                    return a
 
     def post(self):
         json_data = request.json
         advert = Adverts(**json_data)
-        advert = create_advert(advert, session)
+        advert = create_advert(advert)
         return jsonify({"id": advert.id, "date": advert.data_create})
 
-    def delete(self):
-        pass
+    def delete(self, advert_id: int):
+        advert = get_advert(advert_id)
+        request.session.delete(advert)
+        request.session.commit()
+        return jsonify({"status": "deleted"})
 
-advert_view = AdveretsView.as_view("adverts")
+
+
+
+advert_view = AdvertsView.as_view("adverts")
 
 app.add_url_rule("/create_advert", view_func=advert_view, methods=["POST"])
 app.add_url_rule("/list_adverts", view_func=advert_view, methods=["GET"])
-# app.add_url_rule("/advert/<int>", view_func=advert_view, methods=["GET", "DELETE"])
+app.add_url_rule("/advert/<int:advert_id>", view_func=advert_view, methods=["GET", "DELETE"])
 
 
 app.run()
